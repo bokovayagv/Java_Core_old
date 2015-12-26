664c779ad2cc86936ac23dcecae703c0ec99c6c8
bokovayagv <bokovayagv@gmail.com>
2015-12-26 15:50:45 +0300

    dz_1
    Дз по модулю_1

---
+++ Module_1/home/src/mod1.java
@@ -0,0 +1,11 @@
+/**
+ * Created by aleigis on 26.12.2015.
+ */
+public class mod1 {
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+
+
+
+}
